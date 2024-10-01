b488c73f38b761841408d60c2154d88b43001e6d
DiegoDalera <dmdalera@gmail.com>
2024-10-01 19:38:47 -0300

    versionFInal_start

--- procesarArchivos.js
+++ procesarArchivos.js
@@ -8,11 +8,18 @@ function procesarArchivos(sociosFile, prestamosFile, filePath) {
     const sociosWorkbook = XLSX.read(sociosFile, { type: 'buffer' });
     const prestamosWorkbook = XLSX.read(prestamosFile, { type: 'buffer' });
 
+
+    // Seleccionar la primera hoja de cada archivo
     const sociosSheet = sociosWorkbook.Sheets[sociosWorkbook.SheetNames[0]];
     const prestamosSheet = prestamosWorkbook.Sheets[prestamosWorkbook.SheetNames[0]];
 
+    // Convertir las hojas a formato JSON
     const sociosData = XLSX.utils.sheet_to_json(sociosSheet);
-    const prestamosData = XLSX.utils.sheet_to_json(prestamosSheet);
+    const prestamosData = XLSX.utils.sheet_to_json(prestamosSheet, { range: 4 });  // Saltamos las primeras 4 filas
+
+
+    // Filtrar los registros de socios que tienen valores válidos en 'LEGAJO BBVA' y 'Apellido y Nombres'
+    const sociosDataFiltrados = sociosData.filter(row => row['LEGAJO BBVA'] && row['Apellido y Nombres']);
 
     // Crear un mapa para una búsqueda rápida de socios por 'LEGAJO BBVA'
     const sociosMap = sociosData.reduce((map, socio) => {
@@ -24,7 +31,14 @@ function procesarArchivos(sociosFile, prestamosFile, filePath) {
         return map;
     }, {});
 
-    console.log('Mapa de Socios:', sociosMap);
+    //console.log('Mapa de Socios:', sociosMap);
+
+
+    // Filtrar los registros de préstamos que tienen valores válidos en 'LEGAJO'
+    const prestamosDataFiltrados = prestamosData.filter(row => row['LEGAJO']);
+
+    console.log('Prestamos :', prestamosDataFiltrados);
+
 
     // Unir los datos por el número de legajo y asegurarse de que el resultado tenga la cantidad de registros de `prestamosData`
     const mergedData = prestamosData.map(prestamo => {
@@ -44,21 +58,21 @@ function generarArchivoAlta(data, filePath) {
 
     data.forEach(row => {
         const linea = `${(row['NRO. de CUIL'] || '').toString().padStart(11, ' ')}`
-                    + `${(row['TIPO DOC'] || '').toString().padStart(3, ' ')}`
-                    + `${(row['NUMERO'] || '').toString().padStart(20, ' ')}`
-                    + `${(row['Apellido y Nombres'] || '').toString().padStart(70, ' ')}`
-                    + `${(row['Fecha Ing Mutal'] || '').toString().padStart(8, ' ')}`
-                    + `${(row['Sexo'] || '').toString().padStart(1, ' ')}`
-                    + `${(row['ESTADO DE PMOS '] || '').toString().padStart(1, ' ')}`
-                    + `${(row['Domicilio'] || '').toString().padStart(40, ' ')}`
-                    + `${(row['LOCALIDAD'] || '').toString().padStart(20, ' ')}`
-                    + `${(row['PROVINCIA.'] || '').toString().padStart(1, ' ')}`
-                    + `${(row['CODIGO POSTAL'] || '').toString().padStart(8, ' ')}`
-                    + `${(row['TELEFONO_FIJO'] || '').toString().padStart(14, ' ')}`
-                    + `${(row['TELEFONO_CELULAR'] || '').toString().padStart(14, ' ')}`
-                    + `${(row['NACIONALIDAD'] || '').toString().padStart(1, ' ')}`
-                    + `${(row['RETORNO'] || '').toString().padStart(2, ' ')}`
-                    + `${''.padStart(69, ' ')}\r\n`;  // Agregar espacios en blanco para completar los 300 caracteres
+            + `${(row['TIPO DOC'] || '').toString().padStart(3, ' ')}`
+            + `${(row['NUMERO'] || '').toString().padStart(20, ' ')}`
+            + `${(row['Apellido y Nombres'] || '').toString().padStart(70, ' ')}`
+            + `${(row['Fecha Ing Mutal'] || '').toString().padStart(8, ' ')}`
+            + `${(row['Sexo'] || '').toString().padStart(1, ' ')}`
+            + `${(row['ESTADO DE PMOS '] || '').toString().padStart(1, ' ')}`
+            + `${(row['Domicilio'] || '').toString().padStart(40, ' ')}`
+            + `${(row['LOCALIDAD'] || '').toString().padStart(20, ' ')}`
+            + `${(row['PROVINCIA.'] || '').toString().padStart(1, ' ')}`
+            + `${(row['CODIGO POSTAL'] || '').toString().padStart(8, ' ')}`
+            + `${(row['TELEFONO_FIJO'] || '').toString().padStart(14, ' ')}`
+            + `${(row['TELEFONO_CELULAR'] || '').toString().padStart(14, ' ')}`
+            + `${(row['NACIONALIDAD'] || '').toString().padStart(1, ' ')}`
+            + `${(row['RETORNO'] || '').toString().padStart(2, ' ')}`
+            + `${''.padStart(69, ' ')}\r\n`;  // Agregar espacios en blanco para completar los 300 caracteres
         stream.write(linea);
     });
 
